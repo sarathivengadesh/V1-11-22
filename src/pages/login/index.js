53190b2cc744ff0f0ca9ac86a34df3856b9ee72f
sarathinagalingam <sarathi@accuknox.com>
2022-11-16 19:57:28 +0530

    adding controler

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import { Container } from "./style";
+import { useForm, Controller } from "react-hook-form";
 function Login(props) {
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+    setValue,
+  } = useForm();
   const height = window.screen.width;
+  const userData = (data) => {
+    console.log(data);
+  };
   return (
     <Container height={height} className="App">
       <Box
@@ -18,22 +30,64 @@ function Login(props) {
           border: "0.5px solid black",
         }}
       >
-        <Stack direction="column" spacing={4}>
-          <div className="centerAlign">
-            <Typography variant="h4">UserLogin</Typography>
-          </div>
-          <TextField label="useName" color="primary" size="small" />
+        <from onClick={handleSubmit(userData)}>
+          <Stack direction="column" spacing={4}>
+            <div className="centerAlign">
+              <Typography variant="h4">UserLogin</Typography>
+            </div>
 
-          <TextField
-            label="password"
-            type="password"
-            size="small"
-            color="primary"
-          />
-          <Button variant="contained" color="primary">
-            LogIn
-          </Button>
-        </Stack>
+            <Controller
+              {...register("username", {
+                required: "type userName",
+              })}
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, ref } }) => (
+                <TextField
+                  label="useName"
+                  color="primary"
+                  size="small"
+                  value={value}
+                  onChange={(e) => {
+                    onChange(e.target.value);
+                  }}
+                />
+              )}
+            />
+            <Controller
+              {...register("password", {
+                required: "type password",
+              })}
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value, ref },
+                fieldState: { invalid, isTouched, isDirty, error },
+              }) => (
+                <TextField
+                  label="password"
+                  type="password"
+                  size="small"
+                  color="primary"
+                  value={value}
+                  onChange={(e) => {
+                    onChange(e.target.value);
+                  }}
+                  // error={value.length === 0}
+                  // helperText={isDirty && "incorrect"}
+                />
+              )}
+            />
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleSubmit(userData)}
+            >
+              LogIn
+            </Button>
+          </Stack>
+        </from>
       </Box>
     </Container>
   );
